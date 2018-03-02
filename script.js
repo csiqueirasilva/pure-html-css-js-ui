3c90dd900daf6b4e03816cab69e6b3c8f2eba7ea
Caio Siqueira da Silva <csiqueirasilva@gmail.com>
2018-03-02 02:35:58 -0300

    Added custom checkbox to 'pedido'

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ var escolhaPlanoSelect = document.getElementById('escolha-plano-select');
 for(var i = 0; i < escolhaPlanoSelect.children.length; i++) {
 	escolhaPlanoSelect.children[i].innerHTML = escolhaPlanoSelect.children[i].innerHTML.replace(/ /g, '&nbsp;');
 }
+
+document.getElementById('pedido-checkbox-custom').onclick = function() {
+	var el = document.getElementById('pedido-checkbox-acordo');
+	el.checked = !el.checked;
+};
